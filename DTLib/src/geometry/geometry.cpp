dffd2412449b0ef8b7541fc2fd1156282dbde202
fldon <121126066+fldon@users.noreply.github.com>
2025-03-08 20:49:28 +0100

    Missing src file for last commit

--- DTLib/src/geometry/geometry.cpp
+++ DTLib/src/geometry/geometry.cpp
@@ -49,11 +49,10 @@ Vector3d Sphere::get_surface_normal_at(const Point3 &surface_point) const
 
 /* TRIANGLE */
 std::vector< double > Triangle::intersect_ray(const Vector3d &origin,
-                                              const Vector3d &direction) const
+                                            const Vector3d &direction) const
 {
-    //TODO: Why does the moeller-trumbore work but my simple approach does not???
-    //TODO: also, how does the moller-trumbore work and how does it differ from my approach?
-
+    enum class INTERSECT_STRATEGY{GEOM_SLOW, GEOM_FAST, MT};
+    constexpr INTERSECT_STRATEGY STRATEGY_TO_USE = INTERSECT_STRATEGY::GEOM_FAST;
 
     //Triangle points A,B,C
     //Points in triangle are 0A + b*AB + c*AC
@@ -64,118 +63,139 @@ std::vector< double > Triangle::intersect_ray(const Vector3d &origin,
     //Inequalities must be fulfilled: b>=0, c>=0 and b+c <=1 !
     //Otherwise, no valid intersection!
 
-    //In the other functions, Q is D(irection) and P is O(rigin) and q is t
+    //In the other functions, QP is D(irection) and P is O(rigin) and q is t
 
-    constexpr double epsilon = EPSILON;
+    //Problem with this approach: We solve the whole linear system before knowing whether the solution works or not
+    //In the other approaches, there is an early exit
 
-    /* If I add this check to my own version, the problem disappears. But why can I not check this later, after getting the wrong solution???
+    switch(STRATEGY_TO_USE)
+    {
+    case INTERSECT_STRATEGY::GEOM_SLOW:
     {
 
-    Point3 b = m_p2;
-    Point3 c = m_p3;
-    Point3 a = m_p1;
+        const Vector3d vec_AB = m_p2 - m_p1;
+        const Vector3d vec_AC = m_p3 - m_p1;
+        const Vector3d vec_AO = origin - m_p1;
 
-    Vector3d edge1 = b - a;
-    Vector3d edge2 = c - a;
-    Vector3d ray_cross_e2 = direction.cross(edge2);
-    double det = edge1.dot(ray_cross_e2);
 
-    if (det > -epsilon && det < epsilon)
-        return {};    // This ray is parallel to this triangle.
+        Eigen::Matrix3d matrix_A;
+        matrix_A.col(0) = vec_AB;
+        matrix_A.col(1) = vec_AC;
+        matrix_A.col(2) = -direction;
 
-    double inv_det = 1.0 / det;
-    Vector3d s = origin - a;
-    double u = inv_det * s.dot(ray_cross_e2);
+        //result x is (b,c,q), with q being the potential t value we want, and b and c being u,v from the barycentr. coords u,v,w
+        const Eigen::Vector3d x = matrix_A.fullPivLu().solve(vec_AO);
+        const double b = x(0);
+        const double c = x(1);
+        const double t = x(2);
 
-    if ((u < 0 && abs(u) > epsilon) || (u > 1 && abs(u-1) > epsilon))
+
+        //check for inequalities:
+        const bool test_b = not ( (b < 0 && abs(b) > EPSILON) || (b > 1 && abs(b-1) > EPSILON) );
+        const bool test_c = not ( (c < 0 && abs(c) > EPSILON) || (b + c > 1 && abs(b + c - 1) > EPSILON) );
+        const bool test_t = t > EPSILON;
+        const bool valid_intersect = test_b && test_c && test_t;
+
+        std::vector<double> retvec;
+        if(valid_intersect)
+        {
+            debug_assert(std::abs(1 - b - c) < EPSILON && get_baryctr_coords_at(origin + t * direction).has_value());
+            retvec.push_back(t);
+        }
+
+        return retvec;
+    }break;
+
+        //TODO: This above gemoetric solution seems to give intersection points that fail the inside-out tests! Why?
+        //if I explicitly perform the inside out test here and throw away the intersection if it fails, I see no visual difference...
+        //Maybe literal edge cases?
+
+
+
+        //geometric solution using barycentric coords:
+        //Solution stems from equality of ray equation and plane equation
+        //parameter d is the origin-plane distance used in the plane equation
+    case INTERSECT_STRATEGY::GEOM_FAST:
+    {
+        const Vector3d surface_normal = get_surface_normal_at({});
+
+        //check if ray and plane are parallel
+        const double normal_dot_ray_direction = surface_normal.dot(direction);
+        if(std::abs(normal_dot_ray_direction) < EPSILON)
+        {
+            return {}; //parallel: no intersect
+        }
+
+        //compute d parameter
+        const double d = -surface_normal.dot(m_p1);
+
+        //compute t
+        const double t = -(surface_normal.dot(origin) + d) / normal_dot_ray_direction;
+
+        //check if triangle is behind ray
+        if(t < 0)
+        {
+            return {};
+        }
+
+        //Perform inside-out test to see if intersection point is in triangle
+        if(not get_baryctr_coords_at(origin + t * direction).has_value())
+        {
+            return {};
+        }
+
+        const std::vector<double> resvec = {t};
+        return resvec;
+    }break;
+
+
+        //Moeller-Trumbore-algorithm:
+        //Uses cramers rule with the same equation as in the geometric slow solution
+        //Calculates the solution bit by bit and aborts as soon as solution cannot be true anymore
+    case INTERSECT_STRATEGY::MT:
+    {
+        const Point3 &b = m_p2;
+        const Point3 &c = m_p3;
+        const Point3 &a = m_p1;
+
+        const Vector3d edge1 = b - a;
+        const Vector3d edge2 = c - a;
+        const Vector3d ray_cross_e2 = direction.cross(edge2);
+        const double det = edge1.dot(ray_cross_e2);
+
+        if (det > -EPSILON && det < EPSILON)
+            return {};    // This ray is parallel to this triangle.
+
+        const double inv_det = 1.0 / det;
+        const Vector3d s = origin - a;
+        const double u = inv_det * s.dot(ray_cross_e2);
+
+        if ((u < 0 && abs(u) > EPSILON) || (u > 1 && abs(u-1) > EPSILON))
+            return {};
+
+        const Vector3d s_cross_e1 = s.cross(edge1);
+        const double v = inv_det * direction.dot( s_cross_e1);
+
+        if ((v < 0 && abs(v) > EPSILON) || (u + v > 1 && abs(u + v - 1) > EPSILON))
+            return {};
+
+        // At this stage we can compute t to find out where the intersection point is on the line.
+        const double t = inv_det * edge2.dot(s_cross_e1);
+
+        if (t > EPSILON) // ray intersection
+        {
+            return {t};
+        }
+        else        // This means that there is a line intersection but not a ray intersection.
+        {
+            return {};
+        }
+    }break;
+
+    default:
+        debug_stop();
         return {};
     }
-*/
-    /* It seems like this solution falls apart when there is no real solution.
-     * The value-check does not work because the values for b,c,t are themselves okay,
-     * but the solution they symbolize is simply wrong
-     * Maybe Eigen just produces garbage when a system has no solution?
-    const Vector3d vec_AB = m_p2 - m_p1;
-    const Vector3d vec_AC = m_p3 - m_p1;
-    const Vector3d vec_DO = origin - direction;
-    const Vector3d vec_AO = origin - m_p1;
-
-
-    Eigen::Matrix3d matrix_A;
-    matrix_A.col(0) = vec_AB;
-    matrix_A.col(1) = vec_AC;
-    matrix_A.col(2) = vec_DO;
-
-
-    //result x is (b,c,q), with q being the potential t value we want
-    const Eigen::Vector3d x = matrix_A.colPivHouseholderQr().solve(vec_AO);
-    const double b = x(0);
-    const double c = x(1);
-    const double t = x(2);
-
-
-    //check for inequalities:
-    //bool test = (b < 0 && abs(b) > epsilon) || (b > 1 && abs(b-1) > epsilon);
-    //const bool valid_intersect = b >= 0 && c >= 0 && b + c <= 1 && t >= 0 && test;
-    const bool test_b = not ( (b < 0 && abs(b) > epsilon) || (b > 1 && abs(b-1) > epsilon) );
-    const bool test_c = not ( (c < 0 && abs(c) > epsilon) || (b + c > 1 && abs(b + c - 1) > epsilon) );
-    const bool test_t = t > epsilon;
-    const bool valid_intersect = test_b && test_c && test_t;
-
-    std::vector<double> retvec;
-    if(valid_intersect)
-    {
-        retvec.push_back(t);
-    }
-
-    const Point3 alleged_intersect_point = origin + t* direction;
-    if( valid_intersect && alleged_intersect_point(2) < 0 )
-    {
-        int SOMETHINGWENTWRONG = 5;
-    }
-
-    return retvec;
-*/
-
-
-    //Moeller-Trumbore-algorithm:
-    //TODO: try to understand why this algorithm works
-    const Point3 &b = m_p2;
-    const Point3 &c = m_p3;
-    const Point3 &a = m_p1;
-
-    const Vector3d edge1 = b - a;
-    const Vector3d edge2 = c - a;
-    const Vector3d ray_cross_e2 = direction.cross(edge2);
-    const double det = edge1.dot(ray_cross_e2);
-
-    if (det > -epsilon && det < epsilon)
-        return {};    // This ray is parallel to this triangle.
-
-    const double inv_det = 1.0 / det;
-    const Vector3d s = origin - a;
-    const double u = inv_det * s.dot(ray_cross_e2);
-
-    if ((u < 0 && abs(u) > epsilon) || (u > 1 && abs(u-1) > epsilon))
-        return {};
-
-    const Vector3d s_cross_e1 = s.cross(edge1);
-    const double v = inv_det * direction.dot( s_cross_e1);
-
-    if ((v < 0 && abs(v) > epsilon) || (u + v > 1 && abs(u + v - 1) > epsilon))
-        return {};
-
-    // At this stage we can compute t to find out where the intersection point is on the line.
-    const double t = inv_det * edge2.dot(s_cross_e1);
-
-    if (t > epsilon) // ray intersection
-    {
-        return {t};
-    }
-    else        // This means that there is a line intersection but not a ray intersection.
-    {
-        return {};
-    }
-
 }
+
 /* TRIANGLE */
